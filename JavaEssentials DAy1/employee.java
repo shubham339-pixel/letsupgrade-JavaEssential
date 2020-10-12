4fac6fb03ef77b77cbc63d5154af058df07a8d8f
shubham339-pixel <70780441+shubham339-pixel@users.noreply.github.com>
2020-10-12 15:25:01 +0530

    Add files via upload

---
+++ JavaEssentials DAy1/employee.java
@@ -0,0 +1,30 @@
+package letsupgrade;
+
+public class employee {
+	String name="sourabh";
+	
+	int age=26;
+	String city="chennai";
+	
+	String Name="ram";
+	int Age=56;
+	String City="himachal";
+	
+	
+    void display() {
+    	System.out.println("the name is " + name);
+    	System.out.println("the age is " +age);
+    	System.out.println("the city is"  +city);
+    	
+    	
+    	
+    	
+    }
+    
+    void Display() {
+    	System.out.println("the name is " + Name);
+    	System.out.println("the age is " +Age);
+    	System.out.println("the city is"  +City);
+
+}
+}
---
+++ JavaEssentials DAy1/main.java
@@ -0,0 +1,15 @@
+package letsupgrade;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	 employee e= new employee();
+	 e.display();
+	 
+	 employee e1= new employee();
+	 e1.Display();
+
+	}
+
+}
